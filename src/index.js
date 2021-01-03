0255741617c8182fde230313f4120dbbc11fea61
Matthias <matthias.wilhelm.muller@gmail.com>
2021-01-03 16:12:12 -0300

    Poryect Ready with only react. Added Trump API

--- src/index.js
+++ src/index.js
@@ -1,47 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from 'axios';
 
 class Index extends React.Component {
-    state = {cellinput: " ", todos: [], comment: ""}
+    state = {cellinput: "", todos: [], comment: "Ingresar Todo:", keyvalue: 1}
 
     componentDidMount() {
+
+    }
+
+    async requestTrumpData() {
+        let data = await axios.get("https://api.tronalddump.io/random/quote")
+        return data.data.value
     }
 
 
     handleChangeText = (e) => {
         this.setState({cellinput: e.target.value})
     }
-    handleChangeSubmit = (e) => {
+    handleChangeSubmit = async (e) => {
+        e.preventDefault();
+        let comentary = await this.requestTrumpData()
         if (this.state.cellinput !== "") {
             this.setState({
                 cellinput: "",
                 todos: [...this.state.todos,
-                    {key: this.state.todos.length, text: this.state.cellinput}]
+                    {key: this.state.keyvalue, text: this.state.cellinput}],
+                keyvalue: this.state.keyvalue + 1
+                , comment: comentary
             })
         }
     }
-    checkTodo = () =>{
-        console.log(this)
+    removeTodo = (e) => {
         this.setState(
-            {todos:[]}
-        )
+            {todos: this.state.todos.filter(value => value.key.toString() !== e.target.value)}
+        )/*this.state.Todos.filter(value => value.key !== e.target.value)*/
+
     }
 
     render() {
-
         return (
             <div>
-                <h4>ToDo List!</h4>
-                <input type="text" onChange={this.handleChangeText} value={this.state.cellinput}/>
-                <div>
-                    <input type="submit" value="Agregar" onClick={this.handleChangeSubmit}/>
-                </div>
+                <h4>ToDos List!</h4>
+                <h6>{this.state.comment}</h6>
+                <form action=" " onSubmit={this.handleChangeSubmit}>
+                    <input type="text" onChange={this.handleChangeText} value={this.state.cellinput}/>
+                    <div>
+                        <input type="button" value="Agregar" onClick={this.handleChangeSubmit}/>
+                    </div>
+                </form>
                 {this.state.todos.map(
-                     (val) => {
+                    (val) => {
                         return (
-                            <div key={val.key.toString()}>
-                                <Todos text={val.text} checked={false} checkTodo = {this.checkTodo}/>
-                            </div>
+                            <Todos text={val.text} checked={false} removeTodo={this.removeTodo} key={val.key.toString()}
+                                   id={val.key.toString()}/>
                         );
                     }
                 )}
@@ -51,13 +63,13 @@ class Index extends React.Component {
 }
 
 class Todos extends React.Component {
-    state = {text: this.props.text, checked: this.props.checked}
 
     render() {
         return (
-            <div>
-                <input type="checkbox" onClick={this.props.checkTodo}/>
-                {this.state.text}
+            <div key={this.props.id}>
+                <input type="checkbox" value={this.props.id}/>
+                {this.props.text}
+                <input type="checkbox" onClick={this.props.removeTodo} value={this.props.id}/>
             </div>
         )
     }
