3afc96ed3083ed81a28f4eee283feebfd61610bb
Matthias <matthias.wilhelm.muller@gmail.com>
2021-01-05 01:17:25 +0100

    uwu

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ class Index extends React.Component {
     async requestTrumpData() {
         let data = await axios.get("https://api.tronalddump.io/random/quote")
         return data.data.value
+        siii
     }
 
 
